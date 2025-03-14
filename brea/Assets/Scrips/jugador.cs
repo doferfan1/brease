731e8bddd6e7edffb14562f33c2699d3304f63ec
doferfan1 <dofer.wii@gmail.com>
2025-03-13 23:18:24 -0600

    pelota y jugador
    practica modulo 11

--- brea/Assets/Scrips/jugador.cs
+++ brea/Assets/Scrips/jugador.cs
@@ -1,16 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;   
 using UnityEngine;
 
 public class jugador : MonoBehaviour
 {
+    [SerializeField] public int limiteX = 7;
+    [SerializeField] public float velocidad = 10f;
+
+   
+    Vector3 mousepos2d;
+    Vector3 mousepos3d;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Transform transform;
+        transform = this.gameObject.transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+
+        //mousepos2d = Input.mousePosition;
+        //mousepos2d.z = -Camera.main.transform.position.z;
+        //mousepos3d = Camera.main.ScreenToWorldPoint(mousepos2d);
+
+        //if(Input.GetKey(KeyCode.LeftArrow))
+        //{
+        //    this.transform.Translate(Vector3.up * velocidad * Time.deltaTime);
+        //}
+        //if (Input.GetKey(KeyCode.RightArrow))
+        //{
+        //    this.transform.Translate(Vector3.down * velocidad *  Time.deltaTime);
+        //}
+
+        this.transform.Translate(Input.GetAxis("Horizontal") * Vector3.down * velocidad * Time.deltaTime);
+
+        Vector3 pos = this.transform.position;
+
+        //pos.x = mousepos3d.x;
+        if (pos.x < -limiteX)
+        {
+            pos.x = -limiteX;
+        }   else if (pos.x > limiteX)
+        {
+            pos.x = limiteX;
+        }
+        transform.position = pos;
     }
 }
--- brea/Assets/Scrips/pelota.cs
+++ brea/Assets/Scrips/pelota.cs
@@ -1,18 +1,32 @@
 using UnityEngine;
 
-public class pelota : bloque
+public class pelota : MonoBehaviour
 {
-    Color original;
+ public bool enjuego = false;
+    [SerializeField]public float velocidad = 10;
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-      this.GetComponent<Renderer>().material.color=Color.white;
+        Vector3 posicioninicial= GameObject.FindGameObjectWithTag("jugador").transform.position;
+        posicioninicial.y += 3;
+        this.transform.position = posicioninicial;
+        this.transform.SetParent(GameObject.FindGameObjectWithTag("jugador").transform);
+       
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space)|| Input.GetButton("Submit"))
+        {
+           if (!enjuego)
+            {
+                enjuego = true;
+                this.transform.SetParent(null);
+                this.GetComponent<Rigidbody>().linearVelocity = Vector3.up * velocidad;
+            }
+        }   
     }
 }

35aec77b3b3a484a308084000ddba01a20c975b5
doferfan1 <dofer.wii@gmail.com>
2025-03-14 13:42:41 -0600

    PRACTICA_MOD_13
    SEGUIMIENTO

---
+++ brea/Assets/ScriptableObjects/puntaje alto.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+[CreateAssetMenu(fileName = "puntajealto", menuName = "herramientas/puntajealto", order = 0)]
+public class puntajealto : puntajePersis
+{
+    public int puntaje;
+    public int puntajealtos = 10000;
+}
